dab4b3a9aeee0461b1ecfd83dd1ea3cc6c54273e
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2022-06-20 22:19:06 -0300

    Projeto iniciado.

---
+++ src/test/java/com/api/clientpoint/ClientPointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.clientpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientPointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
