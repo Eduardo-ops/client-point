e4fe9f8bb37c0ba9fccec35bbbcfb800eba8971d
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2022-06-20 22:37:36 -0300

    Entidade customer mapeada.

---
+++ src/main/java/com/api/clientpoint/Model/Customer.java
@@ -0,0 +1,32 @@
+package com.api.clientpoint.Model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Customer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(nullable = false, length = 150)
+	private String name;
+
+	@Column(nullable = false, length = 11)
+	private String cpf;
+
+	@Column()
+	private LocalDateTime registerDate;
+
+}
